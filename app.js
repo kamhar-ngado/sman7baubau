befcd9b9bbba516e688f2dcff690329c450f5509
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-12-06 00:40:11 +0800

    add files

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+// number 1
+function check1() {
+  let ans1D = document.getElementById("ans1A");
+  let radios = document.getElementsByName("MC1");
+  var val = "";
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      val = radios[i].value;
+      break;
+    }
+  }
+
+  if (val == "") {
+    alert("please select choice answer");
+  } else if (val == "correct1") {
+    ans1D.classList.toggle("green");
+    alert("Answer is correct !");
+  } else {
+    ans1D.classList.remove("green");
+    alert("Answer is wrong");
+  }
+}
+
+// number 2
+function check2() {
+  let ans2C = document.getElementById("ans2C");
+  let radios = document.getElementsByName("MC2");
+  var val = "";
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      val = radios[i].value;
+      break;
+    }
+  }
+
+  if (val == "") {
+    console.log("please select choice answer");
+  } else if (val == "correct2") {
+    ans2C.classList.toggle("green");
+    alert("Answer is correct !");
+  } else {
+    ans2C.classList.remove("green");
+    alert("Answer is wrong");
+  }
+}
+
+// number 3
+function check3() {
+  let ans2C = document.getElementById("ans3C");
+  let radios = document.getElementsByName("MC2");
+  var val = "";
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      val = radios[i].value;
+      break;
+    }
+  }
+
+  if (val == "") {
+    console.log("please select choice answer");
+  } else if (val == "correct3") {
+    ans2C.classList.toggle("green");
+    alert("Answer is correct !");
+  } else {
+    ans2C.classList.remove("green");
+    alert("Answer is wrong");
+  }
+}
